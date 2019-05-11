1b7add89e79cc4e720b4ed5bfc0b6409057ac21e
Oskar Jedvert <oskar.jedvert@gmail.com>
2019-05-11 14:07:10 +0200

    now sends correct url to backend

--- extension/popup.js
+++ extension/popup.js
@@ -2,9 +2,14 @@
 TODO: add parameter
 */
 function fetch_related_articles(){
-  $.post( "http://localhost:8080/api/coffee", { url: window.location.toString() }, function( data ) {
-    $("#results").html(data);
-  }, "text");
+  chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
+    function(tabs){
+      var url = tabs[0].url;
+      $.post( "http://localhost:8080/api/coffee", { url: url }, function( data ) {
+        $("#results").html(data);
+      }, "text");
+    }
+  );
 }
 
 $(document).ready(function (){
